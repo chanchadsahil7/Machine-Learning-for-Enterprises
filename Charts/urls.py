66f457ced09041b458aeacf6491c815c174a6dc1
ManojKandala <manojkandala@gmail.com>
2018-01-22 14:32:18 +0530

    Added templates, static and URLs

--- Charts/urls.py
+++ Charts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^home/$', views.home,name="home"),
     url(r'^superadmin/$',views.super_user,name="superadmin"),
     url(r'^superadmin_data/$',views.superadmin_data, name="superadmin_data"),
-    url(r'^upload/$',views.simple_upload, name="upload"),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^dashboard/$', views.dashboard, name='index'),
 ]
